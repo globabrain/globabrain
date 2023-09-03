2e229cc61282cf2c2f5943e001501093aa91c6af
harhfeez <fasasihafis1@gmail.com>
2023-09-03 21:27:24 +0100

    integrated aws s3

--- global/global/settings.py
+++ global/global/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'account',
     'contact',
     'features',
+    'boto3'
 ]
 
 MIDDLEWARE = [
@@ -142,6 +143,16 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AWS_ACCESS_KEY_ID = str(os.getenv("AWS_ACCESS_KEY_ID"))
+AWS_SECRET_ACCESS_KEY = str(os.getenv("AWS_SECRET_ACCESS_KEY"))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv("AWS_STORAGE_BUCKET_NAME"))
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = str(os.getenv("AWS_S3_REGION_NAME"))
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 # Default primary key field type
